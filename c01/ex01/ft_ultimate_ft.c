57828987509271e6db3cf6aa3b9f96ae866305bc
Leonel Aron Saba Quezada <lsaba-qu@c2r1s6.42lausanne.ch>
2022-06-10 15:25:24 +0200

    c01 ex02

---
+++ c01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsaba-qu <lsaba-qu@student.42lausan>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/10 13:57:06 by lsaba-qu          #+#    #+#             */
+/*   Updated: 2022/06/10 13:58:22 by lsaba-qu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+int	main ()
+{
+	int a;
+	int *ptr1;
+	int **ptr2;
+	int ***ptr3;
+	int ****ptr4;
+	int *****ptr5;
+	int ******ptr6;
+	int *******ptr7;
+	int ********ptr8;
+	int *********ptr9;
+	
+	a = 1;
+
+	ptr1 = &a;
+	ptr2 = &ptr1;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	ptr8 = &ptr7;
+	ptr9 = &ptr8;
+
+
+	ft_ultimate_ft(ptr9);
+
+	printf("%d", *********ptr9);
+}
+*/
