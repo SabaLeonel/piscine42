40c4d7596dfbfb2c3889029252eb24cfc7e489b6
Leonel Aron Saba Quezada <lsaba-qu@c2r8s2.42lausanne.ch>
2022-06-25 15:31:05 +0200

    jeanneymar

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -1,48 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lsaba-qu <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/23 19:47:18 by lsaba-qu          #+#    #+#             */
-/*   Updated: 2022/06/23 19:47:21 by lsaba-qu         ###   ########.fr       */
+/*   Created: 2022/06/25 10:13:19 by lsaba-qu          #+#    #+#             */
+/*   Updated: 2022/06/25 15:30:47 by lsaba-qu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int	ft_atoi(char *str)
+char	*ft_strcapitalize(char *str)
 {
 	int	i;
-	int	signe;
-	int	res;
 
-	res = 0;
-	signe = 1;
 	i = 0;
-	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
-		i ++;
-	while (str[i] == '+' || str[i] == '-')
-	{
-		signe *= -1;
-		i ++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		res += str[i] - 48;
-		if (str[i + 1] >= '0' && str[i + 1] <= '9')
-			res *= 10;
-		else
-			break ;
-		i ++;
-	}
-	return (res * signe);
-}
-/*
-int	main()
-{
-	char *c = " ---+-e-+1234ab567";
 
-	printf("%d",ft_atoi(c));
-}*/
+	while (str[i])
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+		{
+			str[i] -= 32 ;
+		}
+		else 
+			str[i] += 32;
+	}
+	return (str);
+}
+
+
+int main ()
+{
+	char c;
+
+	c = *ft_strcapitalize("hello");
+
+	printf("%c\n", c);
+	return (0);
+}
---
+++ c04/ex03/ft_atoi.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsaba-qu <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/23 19:47:18 by lsaba-qu          #+#    #+#             */
+/*   Updated: 2022/06/25 10:02:54 by lsaba-qu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	signe;
+	int	res;
+
+	res = 0;
+	signe = 1;
+	i = 0;
+	while ((str[i] >= '\t' && str[i] <= '\r') || str[i] == ' ')
+		i ++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+				signe *= -1;
+		i ++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res += str[i] - 48;
+		if (str[i + 1] >= '0' && str[i + 1] <= '9')
+			res *= 10;
+		else
+			break ;
+		i ++;
+	}
+	return (res * signe);
+}
+
+/*
+int main (int argc, char **argv)
+{
+	
+	(void) argc;
+	
+	printf("%d",ft_atoi(argv[1]));
+}
+*/
